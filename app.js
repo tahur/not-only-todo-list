a1b7bd31e4a25a8ff6949202281f1336a7b5db73
Tahur <mdtahur@gmail.com>
2021-01-10 20:58:14 +0530

    get request route

--- app.js
+++ app.js
@@ -30,18 +30,27 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function (err) {
-  if (err) {
-    console.log(err)
-  } else
-    console.log("pushed to DB")
-});
+
 
 app.get("/", function (req, res) {
   Item.find({}, function (err, foundItems) {
 
-    res.render("list", { listTitle: "Today", newListItems: foundItems });
-    console.log()
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err)
+        } else
+          console.log("pushed to DB")
+      });
+      res.redirect("/")
+
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+      console.log()
+
+    }
+
+
   });
 
 
