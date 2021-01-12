36bfdc54b7715b6bf665a6866d8ae9dbc9ed498f
Tahur <mdtahur@gmail.com>
2021-01-12 21:51:28 +0530

    Dynamic Route add

--- app.js
+++ app.js
@@ -53,10 +53,43 @@ app.get("/", function (req, res) {
 
   });
 
-
-
-
 });
+
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+}
+
+const List = mongoose.model("List", listSchema);
+
+
+
+app.get("/:customListName", function (req, res) {
+
+  const customListName = req.params.customListName;
+
+  List.findOne({ name: customListName }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        })
+        list.save()
+        res.redirect("/" + customListName)
+
+      } else {
+
+        res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+
+      }
+
+    }
+  })
+
+
+})
 
 app.post("/", function (req, res) {
 
